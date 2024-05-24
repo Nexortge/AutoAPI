6a81d0c3cff9212985cfbe1d451e5672ac36af57
Thomas Wubben <ps237747@summacollege.nl>
2024-05-24 11:52:46 +0200

    UPDATE AUTOCONTROLLER
    LOGGING!!!!!!!!

--- app/Http/Controllers/AutoController.php
+++ app/Http/Controllers/AutoController.php
@@ -6,6 +6,7 @@ use App\Models\Auto;
 use App\Models\Kenmerken;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Log;
 
 class AutoController extends Controller
 {
@@ -14,6 +15,7 @@ class AutoController extends Controller
      */
     public function index()
     {
+        Log::info('Auto index methode aangeroepen');
         return Auto::All();
     }
 
@@ -22,6 +24,8 @@ class AutoController extends Controller
      */
     public function store(Request $request)
     {
+        Log::info('Auto store methode aangeroepen met data: ', $request->all());
+
         $validator = Validator::make($request->all(), [
             'naam' => 'required',
             'merk' => 'required',
@@ -29,10 +33,15 @@ class AutoController extends Controller
         ]);
 
         if ($validator->fails()) {
+            Log::warning('Auto store validatie gefaald: ', $validator->errors()->all());
             return response('{"Foutmelding":"Data niet correct"}', 400)
                 ->header('Content-Type','application/json');
         }
-        else return Auto::create($request->all());
+        else {
+            $auto = Auto::create($request->all());
+            Log::info('Nieuwe auto aangemaakt: ', $auto->toArray());
+            return $auto;
+        }
     }
 
     /**
@@ -40,6 +49,7 @@ class AutoController extends Controller
      */
     public function show(Auto $auto)
     {
+        Log::info('Auto show methode aangeroepen voor auto ID: ' . $auto->id);
         return response($auto, 200)
             ->header('Content-Type','application/json');
     }
@@ -49,15 +59,19 @@ class AutoController extends Controller
      */
     public function update(Request $request, Auto $auto)
     {
+        Log::info('Auto update methode aangeroepen voor auto ID: ' . $auto->id . ' met data: ', $request->all());
+
         $validator = Validator::make($request->all(), [
             'naam' => 'required',
             'merk' => 'required',
         ]);
         if($validator->fails()) {
+            Log::warning('Auto update validatie gefaald: ', $validator->errors()->all());
             return response('{"Foutmelding":" Data niet correct"}', 400)
                 ->header('Content-Type','application/json');
         }
         $auto->update($request->all());
+        Log::info('Auto bijgewerkt: ', $auto->toArray());
         return $auto;
     }
 
@@ -66,30 +80,39 @@ class AutoController extends Controller
      */
     public function destroy(Auto $auto)
     {
+        Log::info('Auto destroy methode aangeroepen voor auto ID: ' . $auto->id);
         $auto->delete();
+        Log::info('Auto verwijderd met ID: ' . $auto->id);
         return response('{"Succes":"Auto verwijderd"}', 200)
             ->header('Content-Type','application/json');
     }
 
     public function showParameters(Request $request)
     {
+        Log::info('Auto showParameters methode aangeroepen met parameters: ', $request->all());
+
         if ($request->has('sort')) {
             switch ($request->sort) {
                 case 'naam':
+                    Log::info('Sorteren op naam');
                     return Auto::orderBy('naam')->get();
                 case 'merk':
+                    Log::info('Sorteren op merk');
                     return Auto::orderBy('merk')->get();
                 default:
+                    Log::warning('Ongeldige sorteer parameter: ' . $request->sort);
                     return response('{"Foutmelding":"Sorteer parameter niet correct"}', 400)
                         ->header('Content-Type', 'application/json');
             }
         }
 
         else if($request->has('merk')) {
+            Log::info('Filteren op merk: ' . $request->merk);
             return Auto::where('merk', $request->merk)->get();
         }
 
         else {
+            Log::info('Geen sorteer of filter parameters gevonden, ophalen alle autos met brandstof kenmerken');
             $auto = Auto::All();
             foreach ($auto as $a) {
                 $brandstof = Kenmerken::where('id', $a->brandstof_id);
