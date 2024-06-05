f395d6f4e71931032b2e16052ea1846af5831376
Nexortge <ps240569@summacollege.nl>
2024-06-05 10:36:19 +0200

    idk final touches

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AutoController;
 
-Route::get('/user', function (Request $request) {
+Route::get('/user', function (Request   $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
